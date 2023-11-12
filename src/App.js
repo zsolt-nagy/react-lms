8067ea662e0af3bf3fa0dee2a056dd3a3c8b71fb
Zsolt Nagy <info@zsoltnagy.eu>
2023-11-12 18:36:29 +0100

    CourseList component and dummy courses state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { useState } from "react";
 import PageHeader from "./Components/PageHeader/PageHeader";
+import CourseList from "./Components/CourseList/CourseList";
 
 function App() {
+    const [courses, setCourses] = useState([
+        {
+            author: "John",
+            title: "JavaScript Fundamentals",
+            modules: [],
+        },
+        {
+            author: "Jane",
+            title: "React Fundamentals",
+            modules: [],
+        },
+    ]);
+
     return (
         <>
             <PageHeader />
-            <PageHeader />
+            <CourseList courses={courses} />
         </>
     );
 }
---
+++ src/Components/CourseList/CourseList.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function CourseList({ courses }) {
+  return (
+    <div>CourseList {JSON.stringify(courses)}</div>
+  );
+}
